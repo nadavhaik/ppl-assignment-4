1fa58d5abbaa276d3510ef06ce241ecc620ac328
Avital999 <avital1199@gmail.com>
2022-06-09 20:01:22 +0300

    isLooser
    looserdefined

--- part3/src/L5/L5-typecheck.ts
+++ part3/src/L5/L5-typecheck.ts
@@ -84,9 +84,9 @@ export const getTypeByName = (typeName: string, p: Program): Result<UDTExp> => {
 // Is te1 a subtype of te2?
 const isSubType = (te1: TExp, te2: TExp, p: Program): boolean =>
     isAnyTExp(te2) ? true :
-    isUserDefinedTExp(te1) && isUserDefinedTExp(te2) ?
-        te1.typeName === te2.typeName ? true : //same user defined TExp
-            getParentsType(te1,p).includes(te2) ? true : false : //te2 is parent type of te1
+    isUserDefinedNameTExp(te1) && isUserDefinedNameTExp(te2) ?
+        te1.typeName === te2.typeName ? true : //same user defined name TExp
+            map((t:TExp):string =>isUserDefinedNameTExp(t)? t.typeName: 'xxxxxxx',getParentsType(te1,p)).includes(te2.typeName) ? true : false :
     isTVar(te1)&&isTVar(te2)&&eqTVar(te1,te2) ? true : //same TVars
     tvarDeref(te1) === tvarDeref(te2) ? true : false
 
